2c37f983edff1b357f78b6368db2ccd88a8cac89
Khushal <khushalkatra172@gmail.com>
2025-05-23 16:02:51 +0530

    created custom categories, seeded them and made sidebar responsive

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -5,6 +5,7 @@ import { Navbar } from "./navbar";
 import Footer from "./footer";
 import { SearchFilters } from "./search-filters";
 import { Category } from "@/payload-types";
+import { CustomCategory } from "./types";
 
 interface Props {
   children: React.ReactNode;
@@ -22,9 +23,10 @@ const Layout = async ({ children }: Props) => {
         exists: false,
       },
     },
+    sort: "name",
   });
 
-  const formattedData = data.docs.map((doc) => ({
+  const formattedData: CustomCategory[] = data.docs.map((doc) => ({
     ...doc,
     subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
       ...(doc as Category),
--- src/app/(app)/(home)/search-filters/category-dropdown.tsx
+++ src/app/(app)/(home)/search-filters/category-dropdown.tsx
@@ -5,9 +5,11 @@ import { Category } from "@/payload-types";
 import { useRef, useState } from "react";
 import { useDropdownPosition } from "./use-dropdown-position";
 import { SubcategoryMenu } from "./subcategories-menu";
+import { CustomCategory } from "../types";
+import Link from "next/link";
 
 interface Props {
-  category: Category;
+  category: CustomCategory;
   isActive: boolean;
   isNavigationHovered?: boolean;
 }
@@ -31,22 +33,35 @@ export const CategoryDropdown = ({
   const onMouseLeave = () => setIsOpen(false);
 
   const dropdownPosition = getDropdownPosition();
+
+  // TODO: Potentially improve mobile experience
+  // const toggleDropDown = () => {
+  //   if(category.subcategories?.docs?.length){
+  //     setIsOpen(!isOpen)
+  //   }
+  // }
   return (
     <div 
         className="relative"
         ref = {dropdownRef}
         onMouseEnter = {onMouseEnter}
         onMouseLeave = {onMouseLeave}
+        // onClick={toggleDropDown}
     >
         <div className="relative">
       <Button
         variant="elevated"
         className={cn(
           "h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black",
-          isActive && !isNavigationHovered && "bg-white border-primary"
+          isActive && !isNavigationHovered && "bg-white border-primary",
+          isOpen && "bg-white border-primary shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] -translate-x-[4px] -translate-y-[4px]"
         )}
       >
+        <Link 
+          href={`/${category.slug === "all" ? "" : category.slug}`}
+        >
         {category.name}
+        </Link>
       </Button>
       {category.subcategories && category.subcategories.length > 0 && (
         <div 
@@ -54,7 +69,7 @@ export const CategoryDropdown = ({
                 "opacity-0 absolute -bottom-3 w-0 h-0 border-l-[10px] border-r-[10px] border-b-[10px] border-l-transparent border-r-transparent border-b-black left-1/2 -translate-x-1/2",
                 isOpen && "opacity-100"
             )}
-        
+            
         />
       )}
       </div>
