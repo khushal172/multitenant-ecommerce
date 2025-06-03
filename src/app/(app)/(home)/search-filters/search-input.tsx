20d6498500e0877ca5a22f623766ddec9127b994
khushal172 <khushalkatra172@gmail.com>
2025-06-03 11:57:55 +0530

    used auth states to show different routes

--- src/app/(app)/(home)/search-filters/search-input.tsx
+++ src/app/(app)/(home)/search-filters/search-input.tsx
@@ -1,9 +1,12 @@
 "use client";
 import { Input } from "@/components/ui/input";
-import { ListFilterIcon, SearchIcon } from "lucide-react";
+import { BookmarkCheckIcon, ListFilterIcon, SearchIcon } from "lucide-react";
 import { CategoriesSidebar } from "./categories-sidebar";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+import Link from "next/link";
 
 interface Props{
     disabled?: boolean;
@@ -12,6 +15,8 @@ interface Props{
 
 export const SearchInput = ({disabled} : Props) => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const trpc = useTRPC();
+    const session = useQuery(trpc.auth.session.queryOptions());
     return (
         <div className="flex items-center gap-2 w-full">
             <CategoriesSidebar open = {isSidebarOpen} onOpenChange={setIsSidebarOpen}/>
@@ -19,7 +24,6 @@ export const SearchInput = ({disabled} : Props) => {
                 <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-500"/>
                 <Input className="pl-8" placeholder="Search Products" disabled = {disabled}/>
             </div>           
-            {/* TODO: ADD categories view all button */}
             <Button
                 variant="elevated"
                 className="size-12 shrink-0 flex lg:hidden"
@@ -27,6 +31,17 @@ export const SearchInput = ({disabled} : Props) => {
             >
                 <ListFilterIcon/>
             </Button>
+            {session.data?.user && (
+                <Button
+                asChild
+                variant="elevated"
+                >
+                    <Link href = "/library">
+                        <BookmarkCheckIcon/>
+                        Library
+                    </Link>
+                </Button>
+            )}
             {/* TODO: ADD library button */}
         </div>
     )
--- src/modules/auth/server/procedures.ts
+++ src/modules/auth/server/procedures.ts
@@ -1,8 +1,8 @@
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import { TRPCError } from "@trpc/server";
-import { headers as getHeaders, cookies as getCookies } from "next/headers";
-import { AUTH_COOKIE } from "../constants";
+import { headers as getHeaders } from "next/headers";
 import { loginSchema, registerSchema } from "../schemas";
+import { generateAuthCookie } from "../utils";
 
 export const authRouter = createTRPCRouter({
   session: baseProcedure.query(async ({ ctx }) => {
@@ -54,15 +54,9 @@ export const authRouter = createTRPCRouter({
         });
       }
 
-      const cookies = await getCookies();
-      cookies.set({
-        name: AUTH_COOKIE,
+      await generateAuthCookie({
+        prefix: ctx.db.config.cookiePrefix,
         value: data.token,
-        httpOnly: true,
-        path: "/",
-        // TODO: Ensure cross-domain cookie sharing
-        // sameSite: "none",
-        // domain: ""
       });
     }),
   login: baseProcedure.input(loginSchema).mutation(async ({ input, ctx }) => {
@@ -80,21 +74,11 @@ export const authRouter = createTRPCRouter({
       });
     }
 
-    const cookies = await getCookies();
-    cookies.set({
-      name: AUTH_COOKIE,
+    await generateAuthCookie({
+      prefix: ctx.db.config.cookiePrefix,
       value: data.token,
-      httpOnly: true,
-      path: "/",
-      // TODO: Ensure cross-domain cookie sharing
-      // sameSite: "none",
-      // domain: ""
     });
 
     return data;
-  }),
-  logout: baseProcedure.mutation(async () => {
-    const cookies = await getCookies();
-    cookies.delete(AUTH_COOKIE);
   }),
 });
