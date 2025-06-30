1da27298cbb5aa717a00270a914eafa223d3b804
khushal172 <khushalkatra172@gmail.com>
2025-06-30 13:06:48 +0530

    stripe integration, webhook route, orders collection and checkout procedure

--- src/modules/checkout/ui/views/checkout-view.tsx
+++ src/modules/checkout/ui/views/checkout-view.tsx
@@ -1,20 +1,25 @@
 "use client";
 import { toast } from "sonner";
 import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useCart } from "../../hooks/use-cart";
 import { useEffect } from "react";
 import { generateTenantURL } from "@/lib/utils";
 import { CheckoutItem } from "../components/checkout-item";
 import { CheckoutSidebar } from "../components/checkout-sidebar";
 import { InboxIcon, LoaderIcon } from "lucide-react";
+import { useCheckoutStates } from "../../hooks/use-checkout-states";
+import { useRouter } from "next/navigation";
 
 interface CheckoutViewProps {
   tenantSlug: string;
 }
 
 export const CheckoutView = ({ tenantSlug }: CheckoutViewProps) => {
-  const { productIds, clearAllCarts, removeProduct } = useCart(tenantSlug);
+  const [states, setStates] = useCheckoutStates();
+  const router = useRouter();
+  const { productIds, clearAllCarts, removeProduct, clearCart } =
+    useCart(tenantSlug);
   const trpc = useTRPC();
   const { data, error, isLoading } = useQuery(
     trpc.checkout.getProducts.queryOptions({
@@ -22,34 +27,57 @@ export const CheckoutView = ({ tenantSlug }: CheckoutViewProps) => {
     })
   );
 
+  const purchase = useMutation(
+    trpc.checkout.purchase.mutationOptions({
+      onMutate: () => {
+        setStates({ success: false, cancel: false });
+      },
+      onSuccess: (data) => {
+        window.location.href = data.url;
+      },
+      onError: (error) => {
+        if(error.data?.code === "UNAUTHORIZED"){
+          router.push("/sign-in");
+        }
+        toast.error(error.message)
+      },
+    })
+  );
+
+  useEffect(() => {
+    if (states.success) {
+      setStates({success: false, cancel: false})
+      clearCart();
+      router.push("/products");
+    }
+  }, [states.success, clearCart, router, setStates]);
+
   useEffect(() => {
     if (error?.data?.code === "NOT_FOUND") {
-      clearAllCarts();
+      clearCart();
       toast.warning("Invalid Products found, Cart Cleared");
     }
-  }, [error, clearAllCarts]);
+  }, [error, clearCart]);
 
-  if(isLoading){
+  if (isLoading) {
     return (
-    <div className="lg:pt-16 pt-4 px-4 lg:px-12">
-
+      <div className="lg:pt-16 pt-4 px-4 lg:px-12">
         <div className="border border-black border-dashed flex items-center justify-center p-8 flex-col gap-y-4 bg-white w-full rounded-lg">
-        <LoaderIcon className="text-muted-foreground animate-spin"/>
+          <LoaderIcon className="text-muted-foreground animate-spin" />
+        </div>
       </div>
-</div>
-    )
+    );
   }
 
-  if(data?.totalDocs === 0){
-   return  (
-    <div className="lg:pt-16 pt-4 px-4 lg:px-12">
-   
-   <div className="border border-black border-dashed flex items-center justify-center p-8 flex-col gap-y-4 bg-white w-full rounded-lg">
-        <InboxIcon />
-        <p className="text-base font-medium">No products found</p>
+  if (data?.totalDocs === 0) {
+    return (
+      <div className="lg:pt-16 pt-4 px-4 lg:px-12">
+        <div className="border border-black border-dashed flex items-center justify-center p-8 flex-col gap-y-4 bg-white w-full rounded-lg">
+          <InboxIcon />
+          <p className="text-base font-medium">No products found</p>
+        </div>
       </div>
-      </div>
-      )
+    );
   }
 
   return (
@@ -75,9 +103,9 @@ export const CheckoutView = ({ tenantSlug }: CheckoutViewProps) => {
         <div className="lg:col-span-3">
           <CheckoutSidebar
             total={data?.totalPrice || 0}
-            onCheckout={() => {}}
-            isCanceled={false}
-            isPending={false}
+            onPurchase={() => purchase.mutate({ tenantSlug, productIds })}
+            isCanceled={states.cancel}
+            disabled={purchase.isPending}
           />
         </div>
       </div>
--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,24 +1,45 @@
 import superjson from "superjson";
-import {initTRPC} from '@trpc/server';
-import config from '@payload-config'
-import { getPayload } from 'payload';
-import {cache} from 'react';
+import { initTRPC, TRPCError } from "@trpc/server";
+import config from "@payload-config";
+import { getPayload } from "payload";
+import { cache } from "react";
+import { headers as getHeaders } from "next/headers";
 
 export const createTRPCContext = cache(async () => {
-    return {userID : 'user_123'};
+  return { userID: "user_123" };
 });
 
-
 const t = initTRPC.create({
-    // @see https://trpc.io//docs/server/data-transformers
+  // @see https://trpc.io//docs/server/data-transformers
 
-    transformer: superjson,
-})
+  transformer: superjson,
+});
 
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
-export const baseProcedure = t.procedure.use(async ({next}) => {
-    const payload = await getPayload({config});
+export const baseProcedure = t.procedure.use(async ({ next }) => {
+  const payload = await getPayload({ config });
 
-    return next({ ctx : {db : payload}});
+  return next({ ctx: { db: payload } });
+});
+
+export const protectedProcedure = baseProcedure.use(async ({ ctx, next }) => {
+  const headers = await getHeaders();
+  const session = await ctx.db.auth({ headers });
+  if (!session.user) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+      message: "Not authenticated",
+    });
+  }
+
+  return next({
+    ctx: {
+      ...ctx,
+      session: {
+        ...session,
+        user: session.user,
+      },
+    },
+  });
 });
