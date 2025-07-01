d0b602b7d9b40e52acb8ec54b37d7adddeb2c82a
khushal172 <khushalkatra172@gmail.com>
2025-07-01 10:32:14 +0530

    library and bug fixes

--- src/modules/home/ui/components/search-filters/search-input.tsx
+++ src/modules/home/ui/components/search-filters/search-input.tsx
@@ -8,41 +8,41 @@ import { useTRPC } from "@/trpc/client";
 import { useQuery } from "@tanstack/react-query";
 import Link from "next/link";
 
-interface Props{
-    disabled?: boolean;
+interface Props {
+  disabled?: boolean;
 }
 
-
-export const SearchInput = ({disabled} : Props) => {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-    const trpc = useTRPC();
-    const session = useQuery(trpc.auth.session.queryOptions());
-    return (
-        <div className="flex items-center gap-2 w-full">
-            <CategoriesSidebar open = {isSidebarOpen} onOpenChange={setIsSidebarOpen}/>
-            <div className="relative w-full">
-                <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-500"/>
-                <Input className="pl-8" placeholder="Search Products" disabled = {disabled}/>
-            </div>           
-            <Button
-                variant="elevated"
-                className="size-12 shrink-0 flex lg:hidden"
-                onClick={() => setIsSidebarOpen(true)}
-            >
-                <ListFilterIcon/>
-            </Button>
-            {session.data?.user && (
-                <Button
-                asChild
-                variant="elevated"
-                >
-                    <Link href = "/library">
-                        <BookmarkCheckIcon/>
-                        Library
-                    </Link>
-                </Button>
-            )}
-            {/* TODO: ADD library button */}
-        </div>
-    )
-}
+export const SearchInput = ({ disabled }: Props) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const trpc = useTRPC();
+  const session = useQuery(trpc.auth.session.queryOptions());
+  return (
+    <div className="flex items-center gap-2 w-full">
+      <CategoriesSidebar open={isSidebarOpen} onOpenChange={setIsSidebarOpen} />
+      <div className="relative w-full">
+        <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-500" />
+        <Input
+          className="pl-8"
+          placeholder="Search Products"
+          disabled={disabled}
+        />
+      </div>
+      <Button
+        variant="elevated"
+        className="size-12 shrink-0 flex lg:hidden"
+        onClick={() => setIsSidebarOpen(true)}
+      >
+        <ListFilterIcon />
+      </Button>
+      {session.data?.user && (
+        <Button asChild variant="elevated">
+          <Link prefetch href="/library">
+            <BookmarkCheckIcon />
+            Library
+          </Link>
+        </Button>
+      )}
+      {/* TODO: ADD library button */}
+    </div>
+  );
+};
--- src/modules/products/ui/components/cart-button.tsx
+++ src/modules/products/ui/components/cart-button.tsx
@@ -1,19 +1,38 @@
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { useCart } from "@/modules/checkout/hooks/use-cart";
+import Link from "next/link";
 
 interface Props {
   tenantSlug: string;
   productId: string;
+  isPurchased?: boolean;
 }
 
-export const CartButton = ({ tenantSlug, productId }: Props) => {
+export const CartButton = ({ tenantSlug, productId, isPurchased }: Props) => {
   const cart = useCart(tenantSlug);
+
+  if (isPurchased) {
+    return (
+      <Button
+        variant="elevated"
+        asChild
+        className="flex-1 font-medium bg-white"
+      >
+        <Link prefetch href={`/library/${productId}`}>
+          View in Library
+        </Link>
+      </Button>
+    );
+  }
 
   return (
     <Button
       variant="elevated"
-      className={cn("flex-1 bg-pink-400", cart.isProductInCart(productId) && "bg-white")}
+      className={cn(
+        "flex-1 bg-pink-400",
+        cart.isProductInCart(productId) && "bg-white"
+      )}
       onClick={() => cart.toggleProduct(productId)}
     >
       {cart.isProductInCart(productId) ? "Remove From Cart" : "Add to Cart"}
